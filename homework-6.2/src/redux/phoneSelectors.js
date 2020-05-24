bc89322a5b39b981961bd048c1f1dcdfa9cfa7d9
WerHawer <werhawer@gmail.com>
2020-05-24 13:39:43 +0300

    add filterMemo

--- homework-6.2/src/redux/phoneSelectors.js
+++ homework-6.2/src/redux/phoneSelectors.js
@@ -1,12 +1,22 @@
+import { createSelector } from "reselect";
+
 export const getContacts = (state) => state.contacts;
 
 export const getFilterValue = (state) => state.filter;
 
-export const getFilteredContacts = (state) => {
-  const contacts = getContacts(state);
-  const filter = getFilterValue(state);
+// export const getFilteredContacts = (state) => {
+//   const contacts = getContacts(state);
+//   const filter = getFilterValue(state);
 
-  return contacts.filter(({ name }) =>
-    name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
+// return contacts.filter(({ name }) =>
+//   name.toLowerCase().includes(filter.toLowerCase())
+//   );
+// };
+
+export const getFilteredContacts = createSelector(
+  [getContacts, getFilterValue],
+  (contacts, filter) =>
+    contacts.filter(({ name }) =>
+      name.toLowerCase().includes(filter.toLowerCase())
+    )
+);
